745402ab3182dbef2b6eb3fee5ef6588721c4439
lisha19 <90469378+lisha19@users.noreply.github.com>
2022-10-14 10:00:09 +0530

    Create 0-1 Knapsack Problem.cpp

---
+++ 0-1 Knapsack Problem.cpp
@@ -0,0 +1,27 @@
+int knapSack(int W, int wt[], int val[], int n) 
+    { 
+      int t[n+1][W+1];
+
+ for(int i=0;i<n+1;i++){
+   for(int j=0;j<W+1;j++)
+  {
+ if(j==0 || i==0)
+                     t[i][j] = 0;
+                     
+            }
+    }
+for(int i=1;i<n+1;i++){
+        for(int j=1;j<W+1;j++)
+        {
+            if(wt[i-1] <= j)
+         t[i][j] = max(val[i-1]+ t[i-1][j-wt[i-1]], t[i-1][j]
+               );
+                else
+                t[i][j] = t[i-1][j];
+               }
+}
+
+      return t[n][W];
+      if(wt[i-1]<=j){
+          t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1])
+      }
